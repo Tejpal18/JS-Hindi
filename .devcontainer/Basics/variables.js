3e285865d3d1c41eff6b2da1a16715215def9857
Tejpal18 <144403565+Tejpal18@users.noreply.github.com>
2023-12-04 10:08:37 +0000

    var,let,const

---
+++ .devcontainer/Basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 14553
+
+let accountEmail="tejpal@gmail.com"
+
+var pass="12345"
+
+
+accountCity="Jaitu"
+
+
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountCity,pass])
