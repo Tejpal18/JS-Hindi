c49c8478b875876dcd3fbb853fe7119019b0437a
Tejpal18 <144403565+Tejpal18@users.noreply.github.com>
2023-12-05 10:10:58 +0000

    conversion

---
+++ .devcontainer/Basics/conversion.js
@@ -0,0 +1,13 @@
+const number=45
+
+console.log(typeof number)
+
+let stringIsnumber=String(number)
+
+console.log(typeof stringIsnumber)
+
+let myself=1
+
+let booleanIsMyself=Boolean(myself)
+
+console.log(booleanIsMyself)
