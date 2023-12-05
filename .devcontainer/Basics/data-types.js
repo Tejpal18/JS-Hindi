baaa9b778f4af7f500053b8583349460705710e2
Tejpal18 <144403565+Tejpal18@users.noreply.github.com>
2023-12-05 11:19:26 +0000

    data-types

---
+++ .devcontainer/Basics/data-types.js
@@ -0,0 +1,38 @@
+// Primitive
+
+// 7 types  : Strings, Number, Boolean, null, undefined, Symbol, bigInt
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id===anotherId)
+
+// Reference ( Non Premitive)
+
+// Array,Objects,Functions
+
+const heroes=["Virat Kohli","MS Dhoni","Sachin Tendulkar"]
+
+let myObj={
+    name : "hitesh",
+    age : 22
+
+}
+
+const myFunction=function(){
+    console.log("helllo")
+}
+
+    console.log(typeof anotherId);
+    console.log(typeof myFunction);
+    console.log(typeof heroes);
+
+
+
