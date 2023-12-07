5a119b94956fe3c3712b3e672982e43dd03823b2
Tejpal18 <144403565+Tejpal18@users.noreply.github.com>
2023-12-07 12:36:22 +0000

    nums&maths

---
+++ .devcontainer/Basics/nums&math.js
@@ -0,0 +1,27 @@
+const score =400;
+console.log(score)
+
+const balance=new Number(score)
+console.log(balance)
+
+console.log(balance.toString.length)
+console.log(balance.toFixed(1))
+
+const otherNumber=123.7847598
+
+console.log(otherNumber.toPrecision(3))
+
+const hundereds=10000000
+console.log(hundereds.toLocaleString('en-IN'))
+
+////////// < MATH >> ///////
+
+
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.floor(4.7))
+console.log(Math.ceil(4.2))
+console.log(Math.min(4,3,5,2,8))
+console.log(Math.max(4,3,5,2,8))
+
+console.log((Math.random()*10)+1)
