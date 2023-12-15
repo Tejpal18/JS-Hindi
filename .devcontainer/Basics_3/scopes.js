c89b8fd143389475462bc7c2beacdf0d4a30a57d
Tejpal18 <144403565+Tejpal18@users.noreply.github.com>
2023-12-15 05:58:55 +0000

    nested scopes

--- .devcontainer/Basics_3/scopes.js
+++ .devcontainer/Basics_3/scopes.js
@@ -5,8 +5,52 @@ if(true){
 
     console.log("INNER: ", a);
     
-}
+1}
 
 //console.log(a)
 console.log(b)
 //console.log(j)
+
+function one(){
+    const username="Tejpal"
+
+    function two(){
+        const website="CrickyPoint"
+        console.log(username);
+
+    }
+    //console.log(website)
+    two()
+}
+
+//one()
+
+if(true){
+
+    const username="hitesh"
+    if(username="hitesh"){
+
+        const website="CrickyPoint"
+        //console.log(username+website);
+    }
+    // console.log(website);
+}
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+
+
+function addone(num){
+    return num + 1
+}
+console.log(addone(5))
+
+
+
+
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(5)
